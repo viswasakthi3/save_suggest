d2317fc81a4a737b67d5b644b58c98d8c4fc1eab
vs <viswasakthi2@gmail.com>
2024-09-08 19:38:14 +0530

    save suggest mvp 4

--- pages/index.vue
+++ pages/index.vue
@@ -359,6 +359,7 @@
 <div v-if="notification.show" class="fixed bottom-4 right-4 bg-gray-800 text-white px-4 py-2 rounded-md shadow-lg">
     {{ notification.message }}
   </div>
+  <p>!<p>
 </template>
 
 <script setup>
